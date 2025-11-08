317aa1fdd89b6d694e950b81cbeaeb1779c4efbe
Malisha <128911037+Malisha4065@users.noreply.github.com>
2025-11-08 19:31:13 +0000

    initial commit

---
+++ src/main/java/com/example/kafka_order_system_malisha_apd/KafkaOrderSystemMalishaApdApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_order_system_malisha_apd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrderSystemMalishaApdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOrderSystemMalishaApdApplication.class, args);
+	}
+
+}
